5783e2b379ee7e988d108610287d560c633b7a26
Vigneshwaran-star <nanbanenemy143@gmail.com>
2025-03-27 16:48:29 +0530

    Setdevenv

---
+++ car_game/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div className='flex flex-col items-center justify-center min-h-screen text-center text-9xl'>
+  Setting up the dev environment
+</div>
+    </>
+  )
+}
+
+export default App
